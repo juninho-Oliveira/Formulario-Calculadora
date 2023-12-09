c804250899ef09711901d0d75d9911fffa008884
dj <junior.oliveira05@icloud.com>
2023-12-09 13:59:42 -0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Formulario-Calculadora
+
+Formulario Calculadora feito em aula no senai projeto Ford enter
+feito com HTML CSS e Javascript 
+
+<a href="https://formulario-calculadora-one.vercel.app/">Deploy no vercel </a>
+
+<img src="/mobile.gif"  alt="gif" style="width: 400px;">
--- script.js
+++ script.js
@@ -19,26 +19,24 @@ function Calcular() {
         case 'Somar':
                 resultado = n1 + n2
 
-                alert(`o resultado da soma é: ${resultado}`)
+                document.getElementById('resultado').innerHTML = resultado
             break;
         case 'Subtrair':    
                 resultado = n1 - n2
 
-                alert(`o resultado da subtração é: ${resultado}`)
+                document.getElementById('resultado').innerHTML = resultado
             break;
         case 'Multiplicar':
                 resultado = n1 * n2
-
-                alert(`o resultado da multiplicação é: ${resultado}`)
+                document.getElementById('resultado').innerHTML = resultado
             break;
         case 'Dividir':
                 
                 if (n1 === 0 || n2 === 0) {
-                    alert(`não da para dividir por ${0}`)
                     document.getElementById('resultado').innerHTML = `não da para dividir por ${0}`
                 } else {
                     resultado = n1 / n2
-                    alert(`o resultado da divisão é: ${resultado}`)
+                    document.getElementById('resultado').innerHTML = `o resultado da divisão é: ${resultado}`
                 }
                 
             break;
@@ -47,157 +45,7 @@ function Calcular() {
             alert("Opção inválida")
             break;
     }
-
-    //
-    if (resultado !== undefined) {
-        document.getElementById('resultado').innerHTML = resultado
-    }
     
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Calcular() {
-//     event.preventDefault();
-
-
-//     //criação de vatiavel para pegar os dados do formulário html
-//     let n1 = parseInt(document.getElementById('num1').value);
-//     let n2 = parseInt(document.getElementById('num2').value);
-//     let operacao = document.getElementById('operacao').value;
-//     let resultado;
-
-//     // if (isNaN(n1) || isNaN(n2)) {
-//     //     return alert("Escreva apenas números");
-//     // }
-
-//     switch (operacao) {
-//         case 'Somar':
-//             resultado = Somar(n1, n2);
-//             //exibir o resultado em uma alert -> utilizando a interpolação
-//             // alert(`A soma dos dois números é de: ${resultado}`)
-//             break;
-//         case 'Subtrair':
-//             resultado = Subtrair(n1, n2);
-//             // alert(`A soma dos dois números é de: ${resultado}`)
-//             break;
-//         case 'Multiplicar':
-//             resultado = Multiplicar(n1, n2);
-//             // alert(`A soma dos dois números é de: ${resultado}`)
-//             break;
-//         case 'Dividir':
-//             resultado = Dividir(n1, n2);
-//             // alert(`A soma dos dois números é de: ${resultado}`)
-//             break;
-
-//         default:
-//             alert("está dando erro no código")
-//             break;
-//     }
-
-//     document.getElementById('resultado').innerHTML = resultado;
-// }
-
-// function Subtrair(numero01, numero02) {
-//     return numero01 - numero02;
-// }
-
-// function Somar(numero01, numero02) {
-//     return numero01 + numero02;
-// }
-
-// function Multiplicar(numero01, numero02) {
-//     return numero01 * numero02;
-// }
-
-// function Dividir(numero01, numero02) {
-//     // if (numero01 == 0 || numero02 == 0) {
-
-//     //     return("não é possível dividir por 0"
-//     // }
-
-//     // return numero01 / numero02;
-
-//     try {
-//         if ( numero01 == 0 || numero02 == 0) {
-            
-//             throw new Error ("não é possivel dividir por 0")
-//         }      
-
-//         document.getElementById("error").innerHTML = null;
-
-//         return numero01 / numero02;
-
-//     } catch (err) {
-
-//         document.getElementById("error").innerHTML = err.message;
-
-//     }
-    
-    
-
-// }
